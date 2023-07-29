2b39f9342ee71b3054ac789e46042359d9ec711e
YuTung Liu <yu.t.200053@gmail.com>
2023-07-29 15:25:29 +0800

    7029test

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "/W4-glasses-GGV/",
+  base: "/W4-glasses/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
